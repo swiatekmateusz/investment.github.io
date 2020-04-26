fefdd9d6c7c3027c0812b123416d9730f9f30799
swiatekmateusz <swiatek.mateusz.m@gmail.com>
2020-04-26 17:55:18 +0200

    Update 3

--- src/js/menu.js
+++ src/js/menu.js
@@ -7,8 +7,16 @@ $('[data-togglemenu]').on('click', menuOnClick);
 
 
 function menuOnScroll() {
+  // Fixed menu
   if (window.scrollY > 300) $('.mainmenu').addClass('mainmenu-fixed');
   else $('.mainmenu').removeClass('mainmenu-fixed');
+
+  // Active link
+  $('.nav-desktop .mainmenu__link').each((i, item) => {
+    const $item = $(item);
+    const section = $item.attr('href').substr(1);
+    console.log(section);
+  });
 }
 
 $(document).on('scroll', menuOnScroll);
