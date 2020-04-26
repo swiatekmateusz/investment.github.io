52f6e1b7320adc65277ac5fab39713ecbd94129a
swiatekmateusz <swiatek.mateusz.m@gmail.com>
2020-04-26 17:11:34 +0200

    Update 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 // include ./libs/jquery.js
 // include ./libs/bootstrap.min.js
 // include ./libs/slick.js
+// include ./libs/photoswipe-ui-default.min.js
+// include ./libs/photoswipe.min.js
+// include ./gallery.js
 // include ./slider.js
 // include ./menu.js
