034221f91d84a62e3a2e8213bec7ee5a1fb0be22
swiatekmateusz <swiatek.mateusz.m@gmail.com>
2020-08-01 17:48:21 +0200

    Poprawki

--- src/js/custom.js
+++ src/js/custom.js
@@ -9,3 +9,37 @@ if (barClose) {
     phone.classList.toggle('down');
   });
 }
+
+const residence = document.querySelector('.present__item__residence');
+
+const park = document.querySelector('.present__item__park');
+
+
+if (residence && park) {
+  residence.addEventListener('mouseover', (e) => {
+    residence.classList.add('present__item-hover');
+    park.classList.add('present__item-nothover');
+  });
+  residence.addEventListener('mouseout', (e) => {
+    residence.classList.remove('present__item-hover');
+    park.classList.remove('present__item-nothover');
+  });
+  park.addEventListener('mouseover', (e) => {
+    park.classList.add('present__item-hover');
+    residence.classList.add('present__item-nothover');
+  });
+  park.addEventListener('mouseout', (e) => {
+    park.classList.remove('present__item-hover');
+    residence.classList.remove('present__item-nothover');
+  });
+  // items.forEach((item) => {
+  //   item.addEventListener('mouseover', (e) => {
+  //     item.classList.add('present__item-hover');
+
+  //   });
+  //   item.addEventListener('mouseout', (e) => {
+  //     item.classList.remove('present__item-hover');
+  //   });
+  // });
+  // document.querySelector('.present').addEventListener('mouseout')
+}
