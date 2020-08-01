1ecf110728ae62bd3a77f38b1f3daac585669dcf
swiatekmateusz <swiatek.mateusz.m@gmail.com>
2020-08-01 19:36:57 +0200

    Poprawki

--- src/js/custom.js
+++ src/js/custom.js
@@ -16,21 +16,27 @@ const park = document.querySelector('.present__item__park');
 
 
 if (residence && park) {
+  const logo = document.querySelector('.present__logo');
+
   residence.addEventListener('mouseover', (e) => {
     residence.classList.add('present__item-hover');
     park.classList.add('present__item-nothover');
+    logo.classList.add('present__logo-right');
   });
   residence.addEventListener('mouseout', (e) => {
     residence.classList.remove('present__item-hover');
     park.classList.remove('present__item-nothover');
+    logo.classList.remove('present__logo-right');
   });
   park.addEventListener('mouseover', (e) => {
     park.classList.add('present__item-hover');
     residence.classList.add('present__item-nothover');
+    logo.classList.add('present__logo-left');
   });
   park.addEventListener('mouseout', (e) => {
     park.classList.remove('present__item-hover');
     residence.classList.remove('present__item-nothover');
+    logo.classList.remove('present__logo-left');
   });
   // items.forEach((item) => {
   //   item.addEventListener('mouseover', (e) => {
